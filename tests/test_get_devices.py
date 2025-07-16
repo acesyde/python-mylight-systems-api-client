58d62efa8417ec78ae1ea2bc916e2255bb65e298
Pierre-E Mercier <acesyde@gmail.com>
2025-07-16 20:59:26 +0200

    feat: update project to uv

--- tests/test_get_devices.py
+++ tests/test_get_devices.py
@@ -33,7 +33,7 @@ async def test_get_devices_return_non_2xx_status_code_raise_error(
 ) -> None:
     """Test status call."""
     responses.get(
-        f"{_DEVICE_URL}?authToken=fake-token",
+        str(f"{_DEVICE_URL}?authToken=fake-token"),
         status=status,
     )
     with pytest.raises(MyLightSystemsError):
@@ -46,7 +46,7 @@ async def test_get_devices_with_bad_token_raise_error(
 ) -> None:
     """Test bad token call."""
     responses.get(
-        f"{_DEVICE_URL}?authToken=fake-token",
+        str(f"{_DEVICE_URL}?authToken=fake-token"),
         status=200,
         body=load_fixture("unauthorized.json"),
     )
@@ -61,7 +61,7 @@ async def test_get_devices_success_return_devices(
 ) -> None:
     """Test get profile call."""
     responses.get(
-        f"{_DEVICE_URL}?authToken=fake-token",
+        str(f"{_DEVICE_URL}?authToken=fake-token"),
         status=200,
         body=load_fixture("devices.json"),
     )
